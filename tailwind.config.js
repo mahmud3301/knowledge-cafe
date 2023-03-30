41133f1b3683ec513d57c3b7d3136d748e8cedd1
mahmud3301 <mahmudulhasan13189@gmail.com>
2023-03-30 21:46:24 +0600

    Fake data recived

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,31 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
 
+          "primary": "#cde84a",
+
+          "secondary": "#becc24",
+
+          "accent": "#cd4eed",
+
+          "neutral": "#171622",
+
+          "base-100": "#31373A",
+
+          "info": "#64BBF2",
+
+          "success": "#23E76E",
+
+          "warning": "#AC790C",
+
+          "error": "#F52F19",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
